daedebe969417d0509cd44048d5e3513aacf4ed9
antonova.u <antonova.u@ati.su>
2024-07-23 20:48:48 +0300

    add deploy project files

--- src/typeorm/data-source.ts
+++ src/typeorm/data-source.ts
@@ -7,21 +7,15 @@ import { Transaction } from 'src/transactions/entities/transaction.entity';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  // url: `${process.env.DB_URL}`,
-  // delete after moving database
-  host: `${process.env.DB_HOST}`,
-  port: parseInt(`${process.env.DB_PORT}`),
-  username: `${process.env.DB_USERNAME}`,
-  password: `${process.env.DB_PASSWORD}`,
-  database: `${process.env.DB_DATABASE}`,
+  url: `${process.env.DB_URL}`,
   synchronize: true,
   logging: true,
   entities: [User, Category, Account, Transaction],
   migrations: [],
   subscribers: [],
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 export default dataSourceOptions;
